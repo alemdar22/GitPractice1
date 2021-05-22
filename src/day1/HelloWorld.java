36b92fc35a7025dd5c063acf9fb5579700d3ab6a
alemdar1 <alemdarsu@gmail.com>
2021-05-22 11:17:25 -0400

    saturday addition

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
         System.out.println("Adding new line of code");
         System.out.println("new line");
         System.out.println("here is new push");
+        System.out.println("saturday");
     }
 }
